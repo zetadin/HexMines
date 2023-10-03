93ca6c99966dae15b8c6414b9634d0d4d25a3021
Yuriy Khalak <khalak.yuriy@gmail.com>
2023-10-03 12:25:31 -0300

    initial commit

---
+++ HexMines/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout, get_user_model
+from django.contrib import messages
+from django.http import HttpResponse
+from django.template import loader
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
+
+
+def minefield_view(request):
+    template = loader.get_template('minefield.html')
+    context={}
+    return HttpResponse(template.render(context, request))
